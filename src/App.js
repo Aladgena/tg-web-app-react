5a6d0b07521fac0b4cf1f2032e0ee108aba6230c
Aladgena <aladgena@gmail.com>
2023-11-24 09:17:46 +0300

    commit 3

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import { useEffect } from 'react';
 import './App.css';
+import { useTelegram } from './hooks/useTelegram';
+const {user, onClose} = useTelegram()
 
 function App() {
+	const {onToggleButton, tg} = useTelegram()
+
 	useEffect( () => {
 		tg.ready()
 	})
-
   return (
     <div className="App">
-			WORK
-				<button onClick={onClose}>Закрыть</button>
+			<button onClick={onToggleButton}>Toggle</button>
+
     </div>
   );
 }
